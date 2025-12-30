c88165f7e3b4e1dd326dfc762125dc56ab197a12
NNikhil-Nik <nikhilnikvarshney007@gmail.com>
2025-12-31 02:13:31 +0530

    Email Change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/create-workflow" element={<CreateWorkFlow />} />
-          <>hellpo</>
         </Routes>
       </BrowserRouter>
     </div>
